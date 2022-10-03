d79d1e9b0d4982bdff6569a55e9e5a138e221007
madhavirkl84 <108069586+madhavirkl84@users.noreply.github.com>
2022-10-03 16:18:44 +0530

    Update DriverManager.java

--- src/main/java/driverManger/DriverManager.java
+++ src/main/java/driverManger/DriverManager.java
@@ -3,6 +3,8 @@ package driverManger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,17 +18,20 @@ public WebDriver getDriver() {
 	
 	if (driver==null) {
 		
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver(new FirefoxOptions());
+		
+		//WebDriverManager.chromedriver().setup();
+		//ChromeOptions options = new ChromeOptions();
+		//options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
 		//options.addArguments("--headless");
-		options.setExperimentalOption("useAutomationExtension", false);
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-		options.addArguments("--disable-gpu"); // applicable to windows os only
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		driver = new ChromeDriver(options);
+		//options.setExperimentalOption("useAutomationExtension", false);
+		//options.addArguments("start-maximized"); // open Browser in maximized mode
+		//options.addArguments("disable-infobars"); // disabling infobars
+		//options.addArguments("--disable-extensions"); // disabling extensions
+		//options.addArguments("--disable-gpu"); // applicable to windows os only
+		//options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		//driver = new ChromeDriver(options);
 		//driver = new ChromeDriver(new ChromeOptions());
 	}
 	return driver;
