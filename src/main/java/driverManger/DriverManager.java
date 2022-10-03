88dc265a1aa9b2e5196224125539380aff0c8f0e
madhavirkl84 <108069586+madhavirkl84@users.noreply.github.com>
2022-10-03 15:53:25 +0530

    Update DriverManager.java

--- src/main/java/driverManger/DriverManager.java
+++ src/main/java/driverManger/DriverManager.java
@@ -18,8 +18,13 @@ public WebDriver getDriver() {
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-dev-shm-usage");
 		//disable-dev-shm-usage
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); // Bypass OS security model
 		driver = new ChromeDriver(options);
 		//driver = new ChromeDriver(new ChromeOptions());
 	}
