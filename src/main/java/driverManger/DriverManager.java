343d3789c784740551e7d6b4500bd4168f169046
madhavirkl84 <108069586+madhavirkl84@users.noreply.github.com>
2022-10-03 16:02:28 +0530

    Update DriverManager.java

--- src/main/java/driverManger/DriverManager.java
+++ src/main/java/driverManger/DriverManager.java
@@ -18,14 +18,14 @@ public WebDriver getDriver() {
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
-		//disable-dev-shm-usage
+		options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
+		options.addArguments("--headless");
+		options.setExperimentalOption("useAutomationExtension", false);
 		options.addArguments("start-maximized"); // open Browser in maximized mode
 		options.addArguments("disable-infobars"); // disabling infobars
 		options.addArguments("--disable-extensions"); // disabling extensions
 		options.addArguments("--disable-gpu"); // applicable to windows os only
 		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
-		options.addArguments("--headless"); // Bypass OS security model
 		driver = new ChromeDriver(options);
 		//driver = new ChromeDriver(new ChromeOptions());
 	}
