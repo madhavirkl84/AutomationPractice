f4e31975e511dff630681ad57b2494bac0d1b464
madhavirkl84 <108069586+madhavirkl84@users.noreply.github.com>
2022-10-13 11:16:50 +0530

    Update PageObjectSlidecheck.java

--- src/main/java/pageObjects/PageObjectSlidecheck.java
+++ src/main/java/pageObjects/PageObjectSlidecheck.java
@@ -50,7 +50,7 @@ public class PageObjectSlidecheck {
 	
 	public void clickShop() throws InterruptedException {
 		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		Thread.sleep(500);
+		Thread.sleep(2000);
 		driver.findElement(shoplink).click();
 		//assertTrue(getNav().getText().contains("Shop"));
 		
